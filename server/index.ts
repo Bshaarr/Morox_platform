2950aa3d02f4df5eceb78e7ec6c641e118c8a9bc
Bshaarr <bsharabomorad0@gmail.com>
2025-08-26 01:41:54 +0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,19 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import express from "express";
+import path from "path";
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "../dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
